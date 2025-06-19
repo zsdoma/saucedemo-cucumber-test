4c32d4565cdf213d909bf5c0e8e7473812c11a43
Zsolt Doma <352647+zsdoma@users.noreply.github.com>
2025-06-19 09:41:17 +0200

    headless mode by system prop

--- src/test/java/hu/unideb/inf/zsdoma/saucedemo/cucumbertest/AbstractStepDefs.java
+++ src/test/java/hu/unideb/inf/zsdoma/saucedemo/cucumbertest/AbstractStepDefs.java
@@ -1,9 +1,11 @@
 package hu.unideb.inf.zsdoma.saucedemo.cucumbertest;
 
 import java.time.Duration;
+import java.util.Objects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -14,9 +16,23 @@ public abstract class AbstractStepDefs {
 
   protected static HomePage homePage;
 
+  private static boolean headless = false;
+
   static {
     WebDriverManager.firefoxdriver().setup();
-    driver = new FirefoxDriver();
+
+    String headlessProperty = System.getProperty("selenium.firefox.headless");
+    if (Objects.nonNull(headlessProperty)) {
+      headless = Boolean.parseBoolean(headlessProperty);
+    }
+
+    if (headless) {
+      FirefoxOptions options = new FirefoxOptions();
+      options.addArguments("--headless");
+      driver = new FirefoxDriver(options);
+    } else {
+      driver = new FirefoxDriver();
+    }
 
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WAIT_TIME));
     homePage = new HomePage(driver);
